82f4ce828f8f8e4195a83691e22b0cf038bd27f1
Raghav <raghavwandre1234@gmail.com>
2021-10-23 12:30:25 +0530

    Add: load the data of indian state census Analyser

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Indian_StatesCensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to indian state census analyser");
+        }
+    }
+
+}
